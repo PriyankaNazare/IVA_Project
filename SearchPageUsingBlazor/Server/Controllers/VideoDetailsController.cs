ac8db705f7c16b375d4c5b1c5616197e10170d8d
PriyankaNazare <96390540+PriyankaNazare@users.noreply.github.com>
2022-04-25 12:52:00 -0400

    Add files via upload

---
+++ SearchPageUsingBlazor/Server/Controllers/VideoDetailsController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchPageUsingBlazor.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchPageUsingBlazor.Server.Controllers
+{
+    //[ApiController]
+    //[Route("VideoDetail")]
+    public class VideoDetailsController : ControllerBase
+    {
+
+        //private List<VideoDetails> Loaddata()
+        //{
+        //    VideoDetails V1 = new VideoDetails
+        //    {
+        //        ProgramType = "Movie",
+        //        Title = "Avengers",
+        //        AlternateTitles = "Avengers",
+        //        Year = 2012,
+        //        Cast = new string[] { "abc", "xyz" },
+        //        //Producers=[pqr,lmn],
+        //        //Directors=[],
+        //        Description = "This is a  superhero movie"
+        //    };
+
+        //    VideoDetails V2 = new VideoDetails
+        //    {
+        //        ProgramType = "Movie",
+        //        Title = "Avengers",
+        //        AlternateTitles = "Avengers",
+        //        Year = 2012,
+        //        Cast = new string[] { "abc", "xyz" },
+        //        //Producers=[pqr,lmn],
+        //        //Directors=[],
+        //        Description = "This is a  superhero movie"
+        //    };
+        //    VideoDetails V3 = new VideoDetails
+        //    {
+        //        ProgramType = "Movie",
+        //        Title = "Avengers",
+        //        AlternateTitles = "Avengers",
+        //        Year = 2012,
+        //        Cast = new string[] { "abc", "xyz" },
+        //        //Producers=[pqr,lmn],
+        //        //Directors=[],
+        //        Description = "This is a  superhero movie"
+        //    };
+        //    VideoDetails V4 = new VideoDetails
+        //    {
+        //        ProgramType = "Movie",
+        //        Title = "Avengers",
+        //        AlternateTitles = "Avengers",
+        //        Year = 2012,
+        //        Cast = new string[] { "abc", "xyz" },
+        //        //Producers=[pqr,lmn],
+        //        //Directors=[],
+        //        Description = "This is a  superhero movie"
+        //    };
+
+        //    VideoDetails V5 = new VideoDetails
+        //    {
+        //        ProgramType = "Movie",
+        //        Title = "Avengers",
+        //        AlternateTitles = "Avengers",
+        //        Year = 2012,
+        //        Cast = new string[] { "abc", "xyz" },
+        //        //Producers=[pqr,lmn],
+        //        //Directors=[],
+        //        Description = "This is a  superhero movie"
+        //    };
+
+        //    return new List<VideoDetails> { V1, V2, V3, V4, V5 };
+
+
+
+        //}
+        
+        
+        //[HttpGet]
+        //public List<VideoDetails> GetDetails()
+        //{
+
+        //    var details = Loaddata();
+        //    return details;
+        
+        //}
+    }
+
+}
---
+++ SearchPageUsingBlazor/Shared/ProgramDetails.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPageUsingBlazor.Shared
+{
+    public class VideoDetails
+    {
+        public string ProgramType { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public int Year { get; set; }
+        //public string[] Cast { get; set; }
+        //public string[] Producers { get; set; }
+        //public string[] Directors { get; set; }
+        //public string Description { get; set; }
+
+
+    }
+
+    public class ProgramSource
+    {
+        public string ProgramType { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public int year { get; set; }
+    }
+
+    public class ProgramHit
+    {
+        public ProgramSource Source { get; set; }
+        //public float Score { get; set; }
+        //public string Id { get; set; }
+    }
+
+    public class ProgramSearchResult
+    {
+        //public int Took { get; set; }
+        public ProgramHit[] Hits { get; set; }
+    }
+
+    //public class UIResponseDTo
+    //{
+    //    public string ProgramType { get; set; }
+    //    public string Title { get; set; }
+    //}
+
+    //var progresult = await Http.GetFromJsonAsync<ProgramSearchResult>("https://ee.iva-api.com/api/Entertainment/Search/?Title=ABC&token=sdfsdfd");
+
+    //List<UIResponseDTo> arr = new List<UIResponseDTo>();
+    //foreach(var hit from progresult.Hits){
+    //    var res = new UIResponseDTo
+    //    {
+    //        ProgramType = hit.Source.ProgramType,
+    //        Title = hit.Source.Title
+    //    };
+    //     arr.Add(res);
+    //}
+
+    //return this arr to UI;
+}
